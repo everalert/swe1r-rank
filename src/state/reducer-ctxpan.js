53dae41dedbefc9ceca4b6171e93c2161e28f10f
everalert <everalert@gmail.com>
2020-05-16 00:51:36 +1000

    implemented category discrimination to ctxpan

--- src/state/reducer-ctxpan.js
+++ src/state/reducer-ctxpan.js
@@ -1,14 +1,20 @@
 import VAL from './const';
 import { FormatTime, FormatFullTotalTime, FormatRunsPosted } from '../module/format';
 import { TimeNeededForPoints } from '../module/points';
-import { NewCtxItem, NewCtxHeading, /*NewCtxText*/ } from '../module/ctxpan';
+import { NewCtxItem, NewCtxHeading, NewCtxText } from '../module/ctxpan';
 
 
 export const RankingCtxPanFromState = (state) => {
 	let items = [];
-	const lap = VAL.Setting.Lap[state.settings.lap].key;
-	let playerCount = lap === 'ALL' ? Object.keys(state.players).length : Object.keys(state.players).filter(k => state.players[k][`time${lap}`] > 0).length;
-	let runCount = lap === 'ALL' ? state.runs.length : state.runs.filter(r => r.cat === lap).length;
+	let runs = state.settings.overall ? 
+		state.runs :
+		VAL.Setting.Lap[state.settings.lap].key === 'ALL' ?
+			state.runs.filter(r => r.skips===state.settings.skips && r.upgrades===state.settings.upgrades) :
+			state.runs.filter(r => r.laps===VAL.Setting.Lap[state.settings.lap].key && r.skips===state.settings.skips && r.upgrades===state.settings.upgrades);
+	let players = [];
+	runs.forEach(r => { if (players.indexOf(r.player)<0) players.push(r.player) });
+	let playerCount = players.length;
+	let runCount = runs.length;
 	items.push(NewCtxHeading('Stats'));
 	items.push(NewCtxItem('Players',playerCount));
 	items.push(NewCtxItem('Runs',runCount));
@@ -18,10 +24,15 @@ export const RankingCtxPanFromState = (state) => {
 
 export const TrackListCtxPanFromState = (state) => {
 	let items = [];
-	let totals = Object.keys(state.levels).reduce((v,t) => {
-		v.time1L += state.levels[t].best1L;
-		v.time3L += state.levels[t].best3L;
-		v.timeT += state.levels[t].best1L + state.levels[t].best3L;
+	let totals = Object.keys(state.levels).reduce((v,k) => {
+		let t = state.levels[k];
+		const bests = state.settings.overall ?
+			t.bests :
+			t.bests.filter(t => t.skips===state.settings.skips && t.upgrades===state.settings.upgrades);
+		bests.forEach(b => {
+			v[`time${b.laps}`] += b.time;
+			v.timeT += b.time;
+		});
 		return v;
 	}, {timeT:0, time3L:0, time1L:0});
 	items.push(NewCtxHeading('Record Totals'));
@@ -35,21 +46,30 @@ export const TrackListCtxPanFromState = (state) => {
 export const TrackCtxPanFromState = (state) => {
 	const page = state.panel.section ? state.panel.page : state.page;
 	const lap = VAL.Setting.Lap[state.settings.lap].key;
+	const bests = state.levels[page].bests;
 	let items = [];
 	if (Object.keys(state.levels).indexOf(page)>=0) {
 		if (lap === 'ALL' || lap === '3L') {
-			const time = state.levels[page].best3L;
+			const time = bests.filter(t => t.laps==='3L' && t.skips===state.settings.skips && t.upgrades===state.settings.upgrades)[0].time || 3599.99;
 			items.push(NewCtxHeading('3-Lap Milestones'));
-			items.push(NewCtxItem('90 Points',FormatTime(TimeNeededForPoints(time,90))));
-			items.push(NewCtxItem('50 Points',FormatTime(TimeNeededForPoints(time,50))));
-			items.push(NewCtxItem('Baseline',FormatTime(TimeNeededForPoints(time))));
+			if (!state.settings.overall) {
+				items.push(NewCtxItem('90 Points',FormatTime(TimeNeededForPoints(time,90))));
+				items.push(NewCtxItem('50 Points',FormatTime(TimeNeededForPoints(time,50))));
+				items.push(NewCtxItem('Baseline',FormatTime(TimeNeededForPoints(time))));
+			} else {
+				items.push(NewCtxText('A long time.'))
+			}
 		}
 		if (lap === 'ALL' || lap === '1L') {
-			const time = state.levels[page].best1L;
+			const time = bests.filter(t => t.laps==='1L' && t.skips===state.settings.skips && t.upgrades===state.settings.upgrades)[0].time || 3599.99;
 			items.push(NewCtxHeading('1-Lap Milestones'));
-			items.push(NewCtxItem('90 Points',FormatTime(TimeNeededForPoints(time,90))));
-			items.push(NewCtxItem('50 Points',FormatTime(TimeNeededForPoints(time,50))));
-			items.push(NewCtxItem('Baseline',FormatTime(TimeNeededForPoints(time))));
+			if (!state.settings.overall) {
+				items.push(NewCtxItem('90 Points',FormatTime(TimeNeededForPoints(time,90))));
+				items.push(NewCtxItem('50 Points',FormatTime(TimeNeededForPoints(time,50))));
+				items.push(NewCtxItem('Baseline',FormatTime(TimeNeededForPoints(time))));
+			} else {
+				items.push(NewCtxText('A long time.'))
+			}
 		}
 	}
 	return items;
@@ -58,9 +78,15 @@ export const TrackCtxPanFromState = (state) => {
 
 export const PlayerListCtxPanFromState = (state) => {
 	let items = [];
-	const lap = VAL.Setting.Lap[state.settings.lap].key;
-	let playerCount = lap === 'ALL' ? Object.keys(state.players).length : Object.keys(state.players).filter(k => state.players[k][`time${lap}`] > 0).length;
-	let runCount = lap === 'ALL' ? state.runs.length : state.runs.filter(r => r.cat === lap).length;
+	let runs = state.settings.overall ? 
+		state.runs :
+		VAL.Setting.Lap[state.settings.lap].key === 'ALL' ?
+			state.runs.filter(r => r.skips===state.settings.skips && r.upgrades===state.settings.upgrades) :
+			state.runs.filter(r => r.laps===VAL.Setting.Lap[state.settings.lap].key && r.skips===state.settings.skips && r.upgrades===state.settings.upgrades);
+	let players = [];
+	runs.forEach(r => { if (players.indexOf(r.player)<0) players.push(r.player) });
+	let playerCount = players.length;
+	let runCount = runs.length;
 	items.push(NewCtxHeading('Stats'));
 	items.push(NewCtxItem('Players',playerCount));
 	items.push(NewCtxItem('Runs',runCount));
@@ -70,10 +96,16 @@ export const PlayerListCtxPanFromState = (state) => {
 
 export const PlayerCtxPanFromState = (state) => {
 	const page = state.panel.section ? state.panel.page : state.page;
+	const maxRuns = Object.keys(VAL.Id.Level).length * (state.settings.overall ? Math.pow(2,3) : Math.pow(2,1));
+	let runs = state.settings.overall ? 
+		state.runs.filter(r => r.player===page) :
+		VAL.Setting.Lap[state.settings.lap].key === 'ALL' ?
+			state.runs.filter(r => r.player===page && r.skips===state.settings.skips && r.upgrades===state.settings.upgrades) :
+			state.runs.filter(r => r.player===page && r.laps===VAL.Setting.Lap[state.settings.lap].key && r.skips===state.settings.skips && r.upgrades===state.settings.upgrades);
 	let items = [];
-	let totals = state.runs.reduce((v,t) => {
+	let totals = runs.reduce((v,t) => {
 		if (t.player === page) {
-			v[`time${t.cat}`] += t.time;
+			v[`time${t.laps}`] += t.time;
 			v.timeT += t.time;
 			if (t.points >= 50 && t.points < 90) v.pt50++;
 			if (t.points >= 90 && t.points < 100) v.pt90++;
@@ -82,7 +114,7 @@ export const PlayerCtxPanFromState = (state) => {
 		}
 		return v;
 	}, {timeT:0, time3L:0, time1L:0, runT:0, pt50:0, pt90:0, pt100:0});
-	items.push(NewCtxItem('Runs Posted',FormatRunsPosted(totals.runT,50)));
+	items.push(NewCtxItem('Runs Posted',FormatRunsPosted(totals.runT,maxRuns)));
 	items.push(NewCtxHeading('Run Summary'));
 	items.push(NewCtxItem('100 Points',totals.pt100));
 	items.push(NewCtxItem('90+ Points',totals.pt90));
