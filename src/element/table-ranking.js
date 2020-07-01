482a4b1eb5fba915b5487aa5ac5abb24a00c17ed
everalert <everalert@gmail.com>
2020-07-01 11:11:29 +1000

    add rank styling to ranking and track tables

--- src/element/table-ranking.js
+++ src/element/table-ranking.js
@@ -26,7 +26,7 @@ export default (props) => {
 			{ props.data.map((item,i) => {
 				return (<Tilt className='Tilt' options={VAL.Setting.Tilt.TableItem}>
 					<Link to={VAL.Routes.PLAYER.replace(':id',item.id)} onMouseEnter={()=>showDetail('PLAYER',item.id,item.name)} className={`Tilt-inner item rank${item.rank}`} key={i}>
-						<div className='rank'>{Numeral(item.rank).format('0o')}</div>
+						<div className={`rank rank-${item.rank}`}>{Numeral(item.rank).format('0o')}</div>
 						<div className='player'>{item.name}</div>
 						{ Object.entries(item.fields).map((f,i) =>
 							<div key={i} className={VAL.TableFields[f[0]]}>{f[1]}</div>) }
