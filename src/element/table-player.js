0fedda06be4cb3a6305cd8c787829b67f614a6cf
everalert <everalert@gmail.com>
2020-07-02 03:11:19 +1000

    disabled player profile ctxpan mouse events

--- src/element/table-player.js
+++ src/element/table-player.js
@@ -1,23 +1,23 @@
 import VAL from '../state/const'
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+// import Actions from '../state/action';
 import Tilt from 'react-tilt';
-import Actions from '../state/action';
 import Numeral from 'numeral';
 
 export default (props) => {
-	const dispatch = useDispatch();
-	const showDetail = (section,page,title) => {
-		dispatch(Actions.changeCtxPan(section,page));
-		dispatch(Actions.updateCtxPan(title));
-	};
-	const closeDetail = () => {
-		dispatch(Actions.changeCtxPan());
-		dispatch(Actions.updateCtxPan());
-	};
+	// const dispatch = useDispatch();
+	// const showDetail = (section,page,title) => {
+	// 	dispatch(Actions.changeCtxPan(section,page));
+	// 	dispatch(Actions.updateCtxPan(title));
+	// };
+	// const closeDetail = () => {
+	// 	dispatch(Actions.changeCtxPan());
+	// 	dispatch(Actions.updateCtxPan());
+	// };
 	return (
-		<section className='table table-wide player-times' onMouseLeave={()=>closeDetail(props.player)}>
+		<section className='table table-wide player-times' /*onMouseLeave={()=>closeDetail(props.player)}*/>
 			<div className='header'>
 				<div className='track'>TRACK</div>
 				<div className='best-3lap'>3-LAP</div>
@@ -25,7 +25,7 @@ export default (props) => {
 			</div>
 			{ props.data.map((item,i) => {
 				return <Tilt className='Tilt' options={VAL.Setting.Tilt.TableItem}>
-					<Link to={VAL.Routes.TRACK.replace(':id',item.id)} className='Tilt-inner item' onMouseEnter={()=>showDetail('TRACK',item.id,item.name)} key={i}>
+					<Link to={VAL.Routes.TRACK.replace(':id',item.id)} className='Tilt-inner item' /*onMouseEnter={()=>showDetail('TRACK',item.id,item.name)}*/ key={i}>
 						<div className='track'>{item.name}</div>
 						<div className={`time-3lap rank-${item.fields.rank3L}`}>{item.fields.time3L || VAL.Setting.Fallback.Time}</div>
 						<div className={`points-3lap rank-${item.fields.rank3L}`}>{item.fields.pts3L || VAL.Setting.Fallback.Points}</div>
