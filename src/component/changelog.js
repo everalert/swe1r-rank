dad95c1c50dfbb0231ea92e6b28f35e06ebe72de
everalert <everalert@gmail.com>
2020-07-26 17:23:22 +1000

    bugfix changelog keys warning

--- src/component/changelog.js
+++ src/component/changelog.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Changelog from '../state/changelog';
 
 export default () => {
-	return (<main className="changelog">
+	return <main className="changelog">
 		<h1>RacerRank</h1>
 		<h2>ChangeLog</h2>
 		{ Object.keys(Changelog).sort().reverse().map(major => {
 			const output = [];
-			Object.keys(Changelog[major]).sort().reverse().forEach(minor => {
-				output.push(<h4>v{major}.{minor} &ndash; {Changelog[major][minor].Release.toLocaleDateString()}</h4>);
-				output.push(<ul>{Changelog[major][minor].Notes.map(n => <li>{n}</li>)}</ul>);
+			Object.keys(Changelog[major]).sort().reverse().forEach((minor,i) => {
+				output.push(<h4 key={i*2}>v{major}.{minor} &ndash; {Changelog[major][minor].Release.toLocaleDateString()}</h4>);
+				output.push(<ul key={i*2+1}>{Changelog[major][minor].Notes.map((n,i) => <li key={i}>{n}</li>)}</ul>);
 			})
 			return output;
 		}) }
-	</main>)
+	</main>
 };
