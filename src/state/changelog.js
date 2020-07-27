ae37d4d7aec7dbb385d306f0fb8b76bd79863a11
everalert <everalert@gmail.com>
2020-07-27 13:55:58 +1000

    updated changelog notes for new system

--- src/state/changelog.js
+++ src/state/changelog.js
@@ -4,12 +4,16 @@ export default {
 			Release : new Date('2020-04-21'),
 			Notes : [
 				"Initial release.",
-				"Overall, 3-Lap and 1-Lap rankings.",
-				"Points assigned linearly on a 100-point scale for times up to 1.5× WR to 3 decimal places.",
-				"Times ranked without categoric discrimination.",
-				"Added track-level ranking pages.",
-				"Added player profile pages.",
-				"Added switchable light/dark theme."
+				"Feature", [
+					"Overall, 3-Lap and 1-Lap rankings.",
+					"Added player profile pages.",
+					"Added track-level ranking pages.",
+					"Added switchable light/dark theme."
+				],
+				"Balance", [
+					"Times ranked without categoric discrimination.",
+					"Points assigned linearly on a 100-point scale for times up to 1.5× WR to 3 decimal places.",
+				]
 			]
 		}
 	},
@@ -18,47 +22,65 @@ export default {
 			Release : new Date('2020-05-12'),
 			Notes : [
 				"Website moved to podracing.gg",
-				"Added About page.",
-				"Added Changelog.",
-				"Added linkable pages.",
+				"Feature", [
+					"Added About page.",
+					"Added Changelog.",
+					"Added URL-linkable pages."
+				],
 			]
 		},
 		1 : {
 			Release : new Date('2020-05-13'),
 			Notes : [
-				"Changed lap setting to apply globally.",
-				"Changed internal point precision from 3 to 6 decimal places.",
-				"Cosmetic changes to improve visibility.",
+				"Balance", [
+					"Changed internal point precision from 3 to 6 decimal places."
+				],
+				"UI/UX", [
+					"Changed lap setting to apply globally.",
+					"Cosmetic changes to improve visibility."
+				],
 			]
 		},
 		2 : {
 			Release : new Date('2020-05-16'),
 			Notes : [
-				"Added overall rankings.",
-				"Added individual rankings for skips and upgrades combinations.",
+				"Feature", [
+					"Added overall rankings.",
+					"Added individual rankings for skips and upgrades combinations."
+				]
 			]
 		},
 		3 : {
 			Release : new Date('2020-05-19'),
 			Notes : [
-				"Changed point scale to use sliding multiplier between 1.35× and 2.0× WR."
+				"Balance", [
+					"Changed point scale to use sliding multiplier between 1.35× and 2.0× WR."
+				]
 			]
 		},
 		4 : {
 			Release : new Date('2020-07-26'),
 			Notes : [
-				"Feature - Added category trophies to player profiles.",
-				"Feature - Added ranking information to player timesheets.",
-				"Design - New blue/gold color scheme",
-				"Design - Reworked overall design and design elements.",
-				"Design - Reworked highlights on Player profiles.",
-				"Design - Disabled light theme.",
-				"UX/UI - Settings moved to dropdown menu.",
-				"UX/UI - Category title visual clarity improved.",
-				"UX/UI - Lap setting now skips Combined when on an individual track page.",
-				"UX/UI - Individual track pages now show 3-Lap data when the lap setting is Combined.",
-				"Other - Various performance improvements.",
-				"Other - Various bugfixes."
+				"Feature", [
+					"Added category trophies to player profiles.",
+					"Added ranking information to player timesheets."
+				],
+				"Design", [
+					"New blue/gold color scheme",
+					"Reworked overall design and design elements.",
+					"Reworked highlights on Player profiles.",
+					"Disabled light theme."
+				],
+				"UI/UX", [
+					"Settings moved to dropdown menu.",
+					"Category title visual clarity improved.",
+					"Lap setting now skips Combined when on an individual track page.",
+					"Individual track pages now show 3-Lap data when the lap setting is Combined."
+				],
+				"Other", [
+					"Various performance improvements.",
+					"Various bugfixes."
+				]
 			]
 		},
 		5 : {
