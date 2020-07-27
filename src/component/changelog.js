3daf75ae47de574297e2e758ff77f34b4ffe6c04
everalert <everalert@gmail.com>
2020-07-27 13:48:51 +1000

    add nested changelog notes

--- src/component/changelog.js
+++ src/component/changelog.js
@@ -2,6 +2,18 @@ import React from 'react';
 import Changelog from '../state/changelog';
 
 export default () => {
+	const ProcessNotes = (notes, key) => {
+		return <ul key={key}>{
+			notes.map((n,i) => {
+				switch (typeof n) {
+					case "string": return <li key={i}>{n}</li>;
+					case "object": return Array.isArray(n) ? ProcessNotes(n,i) : null;
+					default: return null;
+				}
+			})
+		}</ul>;
+	}
+
 	return <main className="changelog">
 		<h1>RacerRank</h1>
 		<h2>ChangeLog</h2>
@@ -9,7 +21,7 @@ export default () => {
 			const output = [];
 			Object.keys(Changelog[major]).sort().reverse().forEach((minor,i) => {
 				output.push(<h4 key={i*2}>v{major}.{minor} &ndash; {Changelog[major][minor].Release.toLocaleDateString()}</h4>);
-				output.push(<ul key={i*2+1}>{Changelog[major][minor].Notes.map((n,i) => <li key={i}>{n}</li>)}</ul>);
+				output.push(ProcessNotes(Changelog[major][minor].Notes, i*2+1));
 			})
 			return output;
 		}) }
